1c8c92c6df0b655b5ef69414fde85ea6231d0aae
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-22 23:15:28 +0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import '@turistikrota/ui/assets/config.css'
 import '~/styles/globals.css'
+import 'boxicons/css/boxicons.min.css'
+import 'sspin/dist/index.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
