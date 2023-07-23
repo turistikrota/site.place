6b13ff1e0eddcf7a62b69b7a67deaa3f3ddd0c1b
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-23 15:11:05 +0300

    place: add desktop distance group

---
+++ src/components/filter/desktop/PlaceDesktopDistanceGroup.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from 'react-i18next'
+import { DesktopInfoBox } from '@turistikrota/ui/accessibility/info'
+import PlaceFilterDistanceGroup from '../shared/PlaceFilterDistanceGroup'
+import PlaceDesktopFilterSection from './PlaceDesktopFilterSection'
+import PlaceDesktopHead from './PlaceDesktopHead'
+import { usePlaceFilter } from '~/features/place.filter'
+
+export default function PlaceDesktopDistanceGroup() {
+  const { t } = useTranslation('filter.components.distance')
+  const { query, push } = usePlaceFilter()
+
+  const clearDistance = () => {
+    query.filter.distance = undefined
+    push(query)
+  }
+
+  return (
+    <PlaceDesktopFilterSection>
+      <PlaceDesktopHead>
+        <PlaceDesktopHead.Title className='flex'>
+          {t('text')}
+          <DesktopInfoBox>{t('description')}</DesktopInfoBox>
+        </PlaceDesktopHead.Title>
+        {!!query.filter.distance && <PlaceDesktopHead.Clear onClear={clearDistance} />}
+      </PlaceDesktopHead>
+      <PlaceFilterDistanceGroup />
+    </PlaceDesktopFilterSection>
+  )
+}
