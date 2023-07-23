94d0ae92097c5fb8ee989eb9db23a43032a49cb4
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-23 15:17:43 +0300

    place: add desktop filter section

---
+++ src/components/filter/desktop/PlaceFilterSection.tsx
@@ -0,0 +1,23 @@
+import PlaceDesktopCityGroup from './PlaceDesktopCityGroup'
+import PlaceDesktopDistanceGroup from './PlaceDesktopDistanceGroup'
+import PlaceDesktopFeatureGroup from './PlaceDesktopFeatureGroup'
+import PlaceDesktopMinReviewGroup from './PlaceDesktopMinReviewGroup'
+import PlaceDesktopIsPayedGroup from './PlaceDesktopOtherGroup'
+import PlaceDesktopQueryGroup from './PlaceDesktopQueryGroup'
+import PlaceDesktopTimeSpentGroup from './PlaceDesktopTimeSpentGroup'
+import PlaceDesktopTypeGroup from './PlaceDesktopTypeGroup'
+
+export default function PlaceFilterSection() {
+  return (
+    <>
+      <PlaceDesktopCityGroup />
+      <PlaceDesktopDistanceGroup />
+      <PlaceDesktopTypeGroup />
+      <PlaceDesktopFeatureGroup />
+      <PlaceDesktopQueryGroup />
+      <PlaceDesktopTimeSpentGroup />
+      <PlaceDesktopMinReviewGroup />
+      <PlaceDesktopIsPayedGroup />
+    </>
+  )
+}
