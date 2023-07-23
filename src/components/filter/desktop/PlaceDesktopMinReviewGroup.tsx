304eac56ef0f9c3696148ccc2e0dafe1a8e40fa2
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-23 15:12:42 +0300

    place: add desktop min review group

---
+++ src/components/filter/desktop/PlaceDesktopMinReviewGroup.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from 'react-i18next'
+import { DesktopInfoBox } from '@turistikrota/ui/accessibility/info'
+import PlaceFilterReviewGroup from '../shared/PlaceFilterReviewGroup'
+import PlaceDesktopFilterSection from './PlaceDesktopFilterSection'
+import PlaceDesktopHead from './PlaceDesktopHead'
+import { usePlaceFilter } from '~/features/place.filter'
+
+export default function PlaceDesktopMinReviewGroup() {
+  const { t } = useTranslation('filter.components.review')
+  const { query, push } = usePlaceFilter()
+
+  const clearReview = () => {
+    query.filter.minReview = undefined
+    push(query)
+  }
+
+  return (
+    <PlaceDesktopFilterSection>
+      <PlaceDesktopHead>
+        <PlaceDesktopHead.Title className='flex'>
+          {t('text')}
+          <DesktopInfoBox>{t('description')}</DesktopInfoBox>
+        </PlaceDesktopHead.Title>
+        {!!query.filter.distance && <PlaceDesktopHead.Clear onClear={clearReview} />}
+      </PlaceDesktopHead>
+      <PlaceFilterReviewGroup />
+    </PlaceDesktopFilterSection>
+  )
+}
