5aa7eacefa17b473afe03971d7ecd87f9f3f5f3b
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-23 15:14:34 +0300

    place: add desktop query group

---
+++ src/components/filter/desktop/PlaceDesktopQueryGroup.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from 'react-i18next'
+import { DesktopInfoBox } from '@turistikrota/ui/accessibility/info'
+import PlaceFilterQueryGroup from '../shared/PlaceFilterQueryGroup'
+import PlaceDesktopFilterSection from './PlaceDesktopFilterSection'
+import PlaceDesktopHead from './PlaceDesktopHead'
+import { usePlaceFilter } from '~/features/place.filter'
+
+export default function PlaceDesktopQueryGroup() {
+  const { t } = useTranslation('filter.components.query')
+  const { query, push } = usePlaceFilter()
+
+  const clearQuery = () => {
+    query.filter.query = undefined
+    push(query)
+  }
+
+  return (
+    <PlaceDesktopFilterSection>
+      <PlaceDesktopHead>
+        <PlaceDesktopHead.Title className='flex'>
+          {t('text')}
+          <DesktopInfoBox>{t('description')}</DesktopInfoBox>
+        </PlaceDesktopHead.Title>
+        {!!query.filter.distance && <PlaceDesktopHead.Clear onClear={clearQuery} />}
+      </PlaceDesktopHead>
+      <PlaceFilterQueryGroup />
+    </PlaceDesktopFilterSection>
+  )
+}
