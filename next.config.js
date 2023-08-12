65f29b07e344be0bb99633d83ec7512ca4160a02
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-08-12 15:55:49 +0300

    update nextconfig

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { i18n } = require('./next-i18next.config')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   images: {
     domains: ['https://s3.turistikrota.com'],
