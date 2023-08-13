159a773184fff7506853e761de98f45b874d19c2
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-08-13 12:06:02 +0300

    update the i18n configuration with domains

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,5 +3,16 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'tr'],
+    domains: [
+      {
+        domain: 'places.turistikrota.com',
+        defaultLocale: 'en',
+      },
+      {
+        domain: 'yerler.turistikrota.com',
+        defaultLocale: 'tr',
+      },
+    ],
+    localeDetection: false,
   },
 }
