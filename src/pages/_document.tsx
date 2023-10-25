0201492bd27ff22985885fce589eb70eacc53964
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-25 19:59:13 +0300

    feature/analytics: add the config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,15 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script async={true} src='https://www.googletagmanager.com/gtag/js?id=G-6JLF4Y5XMX'></Script>
+        <Script id='google-tag' strategy='afterInteractive'>
+          {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-6JLF4Y5XMX');
+`}
+        </Script>
       </body>
     </Html>
   )
