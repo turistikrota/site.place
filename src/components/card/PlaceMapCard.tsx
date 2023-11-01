64b123e2e3a03c8329efd6b0558a9104a1243edb
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-01 21:24:42 +0300

    feature/initial-map: update the map card

--- src/components/card/PlaceMapCard.tsx
+++ src/components/card/PlaceMapCard.tsx
@@ -30,6 +30,7 @@ export default function PlaceMapCard({ item }: Props) {
         <Carousel
           imageAltPrefix={translations.title}
           images={mapAndSortImages(item.images)}
+          imageClassName='rounded-b-none'
           sizeClassName='h-75 w-75'
         />
 
