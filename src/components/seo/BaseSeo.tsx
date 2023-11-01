0081a09d877757dd161a812f41321fd4b0e08795
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-01 20:09:17 +0300

    refactor/design: fix seo

--- src/components/seo/BaseSeo.tsx
+++ src/components/seo/BaseSeo.tsx
@@ -1,20 +1,20 @@
+import { useTranslation } from 'react-i18next'
+
 const BaseSeo: React.FC = () => {
+  const { t } = useTranslation('common')
   return (
     <>
       <meta name='application-name' content='Turistikrota' />
       <meta name='generator' content='Turistikrota' />
       <meta name='referrer' content='origin-when-cross-origin' />
-      <meta name='author-url' content='https://turistikrota.com' />
-      <meta name='base-url' content='https://turistikrota.com' />
-      <link rel='alternate' hrefLang='en' href='/en' />
-      <link rel='alternate' hrefLang='tr' href='/tr' />
+      <meta name='author-url' content={t('url')} />
+      <meta name='base-url' content={t('url')} />
       <meta name='color-scheme' content='light dark' />
       <meta name='robots' content='index, follow, nocache' />
       <meta name='googlebot' content='index, follow' />
 
       <link rel='icon' href='/favicon.ico' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-
       <meta name='google-play-app' content='app-id=com.turistikrota.app' />
     </>
   )
