72dc264146924d37cc5eadc683dfa1f1311c0a46
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-24 00:54:12 +0300

    feature/ui: fix the list seo

--- src/components/contents/list/ListHead.tsx
+++ src/components/contents/list/ListHead.tsx
@@ -1,13 +1,29 @@
 import { useTranslation } from 'next-i18next'
 import PlaceDesktopSortGroup from '~/components/filter/desktop/PlaceDesktopSortGroup'
+import { usePlaceFilter } from '~/hooks/place.filter'
+import { useListSeo } from '~/hooks/seo'
+
+const minimizeDescription = (description: string): string => {
+  if (description.length < 100) return description
+  const firstDotIndex = description.indexOf('.')
+  if (firstDotIndex > 0) {
+    return description.slice(0, firstDotIndex + 1)
+  }
+  return description.slice(0, 100) + '...'
+}
 
 const ListHead: React.FC = () => {
+  const { query } = usePlaceFilter()
+  const { title, description } = useListSeo({
+    coordinates: query.filter.coordinates,
+    type: query.filter.types,
+  })
   const { t } = useTranslation('place')
   return (
     <section className='flex items-center justify-between w-full border-none pt-0 lg:pt-4 pb-4'>
       <div className='flex flex-col'>
-        <h1 className='text-3xl font-bold text-gray-800 dark:text-gray-300'>{t('list.title')}</h1>
-        <p className='text-sm text-gray-600 dark:text-gray-400'>{t('list.subtitle')}</p>
+        <h1 className='text-3xl font-bold text-gray-800 dark:text-gray-300'>{title}</h1>
+        <p className='text-sm text-gray-600 dark:text-gray-400'>{minimizeDescription(description)}</p>
       </div>
       <PlaceDesktopSortGroup />
     </section>
---
+++ src/utils/turkish.tsx
@@ -0,0 +1,29 @@
+export function locativeSuffix(name: string, apostrophe = true): string {
+  const vowels = 'aeıioöuü'
+  const vowelsBack = 'aıou'
+
+  let lastVowel = null
+  for (let i = name.length - 1; i >= 0; i--) {
+    if (vowels.includes(name[i])) {
+      lastVowel = name[i]
+      break
+    }
+  }
+  let suffix = lastVowel && vowelsBack.includes(lastVowel) ? 'da' : 'de'
+
+  if (apostrophe) {
+    const lastTwoChars = name.slice(-2)
+    if (vowels.includes(lastTwoChars[0]) || lastTwoChars[1] === 'e' || lastTwoChars[1] === 'a') {
+      suffix = "'" + suffix
+    }
+  }
+
+  return name + suffix
+}
+
+export function withLocativeSuffix(locale: string, name: string): string {
+  if (locale === 'tr') {
+    return locativeSuffix(name)
+  }
+  return name
+}
