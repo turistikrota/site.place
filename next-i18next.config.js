68957bb717710df2c747dcfbe5e23dfad4b64802
9ssi7 <info@ssibrahimbas.com>
2023-12-13 02:56:53 +0300

    feature/translations: fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale: 'tr',
     locales: ['en', 'tr'],
     domains: [
       {
