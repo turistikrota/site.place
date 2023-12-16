d8e6dda94c32476add56c6cb9328a61e9093b44a
9ssi7 <info@ssibrahimbas.com>
2023-12-16 12:15:41 +0300

    feature/analytics: set with i18n

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,47 @@ import GlassEffect from '@turistikrota/ui/cjs/design/glass'
 import { useTranslation } from 'next-i18next'
 import { Head, Html, Main, NextScript } from 'next/document'
 import Script from 'next/script'
+import { FC } from 'react'
+
+const TurkishAnalytics: FC = () => {
+  return (
+    <>
+      <Script
+        async={true}
+        src='https://www.googletagmanager.com/gtag/js?id=G-6JLF4Y5XMX'
+        strategy='afterInteractive'
+      ></Script>
+      <Script id='google-tag' strategy='afterInteractive'>
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-6JLF4Y5XMX');
+`}
+      </Script>
+    </>
+  )
+}
+
+const EnglishAnalytics: FC = () => {
+  return (
+    <>
+      <Script
+        async={true}
+        src='https://www.googletagmanager.com/gtag/js?id=G-VX790XP0CV'
+        strategy='afterInteractive'
+      ></Script>
+      <Script id='google-tag' strategy='afterInteractive'>
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-VX790XP0CV');
+`}
+      </Script>
+    </>
+  )
+}
 
 export default function Document() {
   const { i18n } = useTranslation()
@@ -14,19 +55,7 @@ export default function Document() {
         <CubeEffect.All />
         <Main />
         <NextScript />
-        <Script
-          async={true}
-          src='https://www.googletagmanager.com/gtag/js?id=G-6JLF4Y5XMX'
-          strategy='afterInteractive'
-        ></Script>
-        <Script id='google-tag' strategy='afterInteractive'>
-          {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-6JLF4Y5XMX');
-`}
-        </Script>
+        {i18n.language === 'tr' ? <TurkishAnalytics /> : <EnglishAnalytics />}
       </body>
     </Html>
   )
