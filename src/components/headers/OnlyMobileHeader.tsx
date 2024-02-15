261c6792df2699e2077c46f2729df6f069768683
9ssi7 <info@ssibrahimbas.com>
2024-02-15 03:29:43 +0300

    feature/logo: move to new

--- src/components/headers/OnlyMobileHeader.tsx
+++ src/components/headers/OnlyMobileHeader.tsx
@@ -1,11 +1,7 @@
 import MobileHeader from '@turistikrota/ui/headers/mobile'
-import TopHeader from '@turistikrota/ui/headers/top'
-import Logo from '@turistikrota/ui/logo'
 import { useTranslation } from 'next-i18next'
 import dynamic from 'next/dynamic'
-import Link from 'next/link'
-import { getStaticRoute } from '~/static/page'
-import HeaderLogo from './HeaderLogo'
+import ModernLogoProvider from './ModernLogo'
 
 type Props = {
   accessTokenIsExists: boolean
@@ -19,30 +15,9 @@ export default function OnlyMobileHeader({ accessTokenIsExists }: Props) {
   const { t, i18n } = useTranslation('common')
   return (
     <>
-      <TopHeader>
-        <TopHeader.Left>
-          <Link
-            href={getStaticRoute(i18n.language).base}
-            className='hover:opacity-80 transition-all duration-200 ease-in-out'
-          >
-            {t('header.links.home')}
-          </Link>
-        </TopHeader.Left>
-        <TopHeader.Right>
-          <Link
-            href={getStaticRoute(i18n.language).aboutUs}
-            className='hover:opacity-80 transition-all duration-200 ease-in-out'
-          >
-            {t('header.links.aboutUs')}
-          </Link>
-        </TopHeader.Right>
-      </TopHeader>
-
-      <MobileHeader withTopHeader>
+      <MobileHeader defaultFixed>
         <MobileHeader.Left>
-          <HeaderLogo>
-            <Logo width={186} height={30} />
-          </HeaderLogo>
+          <ModernLogoProvider />
         </MobileHeader.Left>
         <MobileHeader.Fill className='hidden md:flex'>{``}</MobileHeader.Fill>
         <MobileHeader.Right>
